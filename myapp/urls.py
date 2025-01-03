78ba99d8bc4801d7aacbef55df5fe08661398349
beeplovebhandari <bbhandari0304@gmail.com>
2025-01-03 17:09:06 +0545

    django project setup

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, python
 
 urlpatterns = [
     path("world/", home),
+    path("python/", python),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello World")
+
+def python(request):
+    return HttpResponse("<h1>I'am Learning Python</h1>")
