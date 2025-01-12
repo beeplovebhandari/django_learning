1b4ac170d1c73d0afaf7b38be9a06a2f30aed66e
beeplovebhandari <bbhandari0304@gmail.com>
2025-01-12 21:43:10 +0545

    pricing

--- temp_inheritance/urls.py
+++ temp_inheritance/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import main, features
+from .views import main, features, pricing
 
 urlpatterns = [
     path('features/', features, name='features'),
+    path('pricing/', pricing, name='pricing'),
     path('', main, name='home')
     
 ]
--- temp_inheritance/views.py
+++ temp_inheritance/views.py
@@ -21,3 +21,17 @@ def features(request):
     ]
     return render (request, template_name='temp_inheritance/features.html', context={"items": items})
 
+
+def pricing(request):
+    prices=[
+        {"laptop":"Acer", "warranty": "1 Years", "price": "NRS 75000", "avilable": "Yes"},
+        {"laptop":"Dell", "warranty": "2 Years", "price": "NRS 50000", "avilable": "Out of Stock"},
+        {"laptop":"Lenevo", "warranty": "-", "price": "NRS 72000", "avilable": "Yes"},
+        {"laptop":"MSI", "warranty": "6 Months", "price": "NRS 85000", "avilable": "Out Of Stock"},
+        {"laptop":"Macbook", "warranty": "1 Years", "price": "NRS 105000", "avilable": "Yes"},
+        {"laptop":"ASUS", "warranty": "1.5 Years", "price": "NRS 92999", "avilable": "Launching Soon"},
+        {"laptop":"HP", "warranty": "-", "price": "NRS 50000", "avilable": "Only 3 Piece avilable"},
+        {"laptop":"Samsung", "warranty": "3 Years", "price": "NRS 45000", "avilable": "Yes"},
+    ]
+    return render (request, template_name='temp_inheritance/pricing.html', context={"prices":prices})
+
