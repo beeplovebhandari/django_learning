1005bca6bdac8e822553b77543534e03b9d50ac0
beeplovebhandari <bbhandari0304@gmail.com>
2025-01-14 21:08:34 +0545

    template

--- tables/views.py
+++ tables/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from.models import Students
+
+def student(request):
+    students = Students.objects.all()
+    return render (request , template_name = "tables/student.html", context={"students": students})
